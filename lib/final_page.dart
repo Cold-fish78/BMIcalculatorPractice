65ec239dff2fd27efc6329a51f8c95021a06d39a
Cold-fish78 <pubg69086@gmail.com>
2022-02-11 22:29:47 +0530

    add second page with help of navigator

---
+++ lib/final_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class FinalPage extends StatelessWidget {
+  const FinalPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+     appBar: AppBar(
+       title: const Text('BMI CALCULTOR'),
+     ),
+      body: Text('hello'),
+    );
+  }
+}
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'columnWidget.dart';
 import 'newWidget.dart';
+import 'final_page.dart';
 
 const activeColor = Color(0xFF1D1E33);
 const InactiveColor = Color(0xFF111328);
@@ -21,7 +22,9 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Color maleCardColor = InactiveColor;
   Color femaleCardColor = InactiveColor;
-  int height = 180;
+  int height = 120;
+  int weight = 60;
+  int age = 19;
   void updateColor(Gender gender) {
     if (gender == Gender.male) {
       if (maleCardColor == InactiveColor) {
@@ -103,7 +106,7 @@ class _InputPageState extends State<InputPage> {
                           crossAxisAlignment: CrossAxisAlignment.baseline,
                           mainAxisAlignment: MainAxisAlignment.center,
                           textBaseline: TextBaseline.alphabetic,
-                          children:  [
+                          children: [
                             Text(
                               height.toString(),
                               style: const TextStyle(
@@ -118,13 +121,27 @@ class _InputPageState extends State<InputPage> {
                             ),
                           ],
                         ),
-                        Slider(
-                          value: height.toDouble(),
-                          onChanged: null,
-                          min: 0,
-                          max: 240,
-                          divisions: 239,
-                          label: ,
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            inactiveTrackColor: const Color(0xFF8D8E98),
+                            thumbShape: const RoundSliderThumbShape(
+                                enabledThumbRadius: 15),
+                            overlayShape: const RoundSliderOverlayShape(
+                                overlayRadius: 30),
+                            thumbColor: const Color(0xFFEB1555),
+                            activeTrackColor: Colors.white,
+                            overlayColor: const Color(0x29EB1555),
+                          ),
+                          child: Slider(
+                            value: height.toDouble(),
+                            min: 120,
+                            max: 240,
+                            onChanged: (double newValue) {
+                              setState(() {
+                                height = newValue.round();
+                              });
+                            },
+                          ),
                         ),
                       ],
                     ),
@@ -138,24 +155,126 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: NewWidget(
-                      Colour: activeColor,
-                      childCard:
-                          ColumnWidget(FontAwesomeIcons.plus, 'tobeUpdated')),
+                    Colour: activeColor,
+                    childCard: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'WEIGHT',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w900, fontSize: 50),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                          backgroundColor: const Color(0xFF4C4F5E),
+                              child:const Icon(FontAwesomeIcons.minus,
+                              color: Colors.white,),
+                              onPressed: (){
+                                setState(() {
+                                  weight --;
+                                });
+
+                              },
+
+                            ),
+                            const SizedBox(width: 10,),
+                            FloatingActionButton(
+                              backgroundColor: const Color(0xFF4C4F5E),
+
+                              child:const Icon(FontAwesomeIcons.plus,
+                              color: Colors.white,),
+                              onPressed: (){
+                               setState(() {
+                                 weight ++;
+                               });
+                              },
+
+                            )
+
+
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ),
                 Expanded(
                   child: NewWidget(
-                      Colour: activeColor,
-                      childCard:
-                          ColumnWidget(FontAwesomeIcons.mars, 'tobeUpdated')),
+                    Colour: activeColor,
+                    childCard: Column(
+                      children: [   const Text(
+                        'AGE',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      ),
+                        Text(
+                          age.toString(),
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w900, fontSize: 50),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FloatingActionButton(
+                              backgroundColor: const Color(0xFF4C4F5E),
+                              child:const Icon(FontAwesomeIcons.minus,
+                                color: Colors.white,),
+                              onPressed: (){
+                                setState(() {
+                                  age --;
+                                });
+
+                              },
+
+                            ),
+                            const SizedBox(width: 10,),
+                            FloatingActionButton(
+                              backgroundColor: const Color(0xFF4C4F5E),
+
+                              child:const Icon(FontAwesomeIcons.plus,
+                                color: Colors.white,),
+                              onPressed: (){
+                                setState(() {
+                                  age ++;
+                                });
+                              },
+
+                            )
+
+
+                          ],
+                        )],
+                    ),
+                  ),
                 )
               ],
             ),
           ),
-          Container(
-            color: Colors.pink,
-            width: double.infinity,
-            height: 80.0,
-            margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => FinalPage()));
+            },
+            child: Container(
+              alignment: Alignment.center,
+              color: Colors.pink,
+              width: double.infinity,
+              height: 80.0,
+              margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Text('CALCULATE',
+              style:const TextStyle(
+                  fontWeight: FontWeight.w900, fontSize: 25),),
+            ),
           )
         ],
       ),
